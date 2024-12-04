140de0b7035439fa0c3922fd098bb463a97bfb85
480284856@qq.com <480284856@qq.com>
2024-12-04 14:35:18 +0800

    bug fix

--- run_container.sh
+++ run_container.sh
@@ -5,3 +5,7 @@ docker build -t visualized_container .
 
 # 运行容器并映射端口
 docker run -d -p 6080:6080 --name visualized_container visualized_container
+
+# 告诉用户
+echo "Container running at http://localhost:6080"
+echo "Password: 123123"
